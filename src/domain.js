1a47826903d33ec766aa8a6ac867a8d09eb0fa72
x412566612 <412566612@qq.com>
2019-08-03 09:40:57 +0800

    vue 前端初次提交

---
+++ src/domain.js
@@ -0,0 +1,15 @@
+//全局的配置的一些服务端的域名
+const serverpath='http://127.0.0.1:8888/'
+//WebSocket实时信息提示功能
+const webSocketUrl='ws://localhost:8888/ws/websocket/'
+//图片上传
+const serverimageupload='http://'
+const userinfo={};
+const websocket=null;
+export default {
+  webSocketUrl,//webSocket配置的url
+  serverpath,  //WebServer的服务端地址
+  userinfo,
+  websocket
+}
+
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/components/login'
+Vue.use(Router)
+
+export default new Router({
+  mode:"history",
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login,
+      meta:{
+        require:false//该路径的访问不需要登录
+      }
+    }
+
+  ]
+})
